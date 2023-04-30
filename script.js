b6a2e9e8221d7effed3ffa2bb5ee919ad825329b
jeon303 <42285735+jeon303@users.noreply.github.com>
2023-04-30 17:07:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
 
-	var apiUrl = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+	var apiUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
-	function getBitcoinPrice() {
+	function getCryptocurrencies() {
 		$.getJSON(apiUrl, function(data) {
-			var bitcoinPrice = data["bitcoin"]["usd"].toLocaleString('en-US', {style: 'currency', currency: 'USD'});
-			$("#price").text(bitcoinPrice);
+			var cryptocurrencies = data;
+
+			$.each(cryptocurrencies, function(index, cryptocurrency) {
+				var rank = cryptocurrency.market_cap_rank;
+				var name = cryptocurrency.name;
+				var symbol = cryptocurrency.symbol.toUpperCase();
+				var price = cryptocurrency.current_price.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+				var marketCap = cryptocurrency.market_cap.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+				var change24h = cryptocurrency.price_change_percentage_24h.toFixed(2);
+				var changeClass = change24h >= 0 ? "green" : "red";
+
+				var cryptocurrencyRow = "<tr>";
+				cryptocurrencyRow += "<td>" + rank + "</td>";
+				cryptocurrencyRow += "<td>" + name + "</td>";
+				cryptocurrencyRow += "<td>" + symbol + "</td>";
+				cryptocurrencyRow += "<td>" + price + "</td>";
+				cryptocurrencyRow += "<td>" + marketCap + "</td>";
+				cryptocurrencyRow += "<td class=\"" + changeClass + "\">" + change24h + "</td>";
+				cryptocurrencyRow += "</tr>";
+
+				$("#cryptocurrencies-table").append(cryptocurrencyRow);
+			});
 		});
 	}
 
-	getBitcoinPrice();
+	getCryptocurrencies();
 
 });
